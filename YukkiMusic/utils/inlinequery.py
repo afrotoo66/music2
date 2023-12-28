f0490b6566fa763d352854413ca635290c9be65c
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2023-12-28 04:16:30 +0200

    Add files via upload

---
+++ YukkiMusic/utils/inlinequery.py
@@ -0,0 +1,72 @@
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from pyrogram.types import (InlineQueryResultArticle,
+                            InputTextMessageContent)
+
+answer = []
+
+answer.extend(
+    [
+        InlineQueryResultArticle(
+            title="Pause Stream",
+            description=f"Pause the current playout on group call.",
+            thumb_url="https://telegra.ph/file/04b2f1f1c808dc49db35b.jpg",
+            input_message_content=InputTextMessageContent("/pause"),
+        ),
+        InlineQueryResultArticle(
+            title="Resume Stream",
+            description=f"Resume the ongoing playout on group call.",
+            thumb_url="https://telegra.ph/file/04b2f1f1c808dc49db35b.jpg",
+            input_message_content=InputTextMessageContent("/resume"),
+        ),
+        InlineQueryResultArticle(
+            title="Mute Stream",
+            description=f"Mute the ongoing playout on group call.",
+            thumb_url="https://telegra.ph/file/04b2f1f1c808dc49db35b.jpg",
+            input_message_content=InputTextMessageContent("/mute"),
+        ),
+        InlineQueryResultArticle(
+            title="Unmute Stream",
+            description=f"Unmute the ongoing playout on group call.",
+            thumb_url="https://telegra.ph/file/04b2f1f1c808dc49db35b.jpg",
+            input_message_content=InputTextMessageContent("/unmute"),
+        ),
+        InlineQueryResultArticle(
+            title="Skip Stream",
+            description=f"Skip to next track. | For Specific track number: /skip [number] ",
+            thumb_url="https://telegra.ph/file/04b2f1f1c808dc49db35b.jpg",
+            input_message_content=InputTextMessageContent("/skip"),
+        ),
+        InlineQueryResultArticle(
+            title="End Stream",
+            description="Stop the ongoing playout on group call.",
+            thumb_url="https://telegra.ph/file/04b2f1f1c808dc49db35b.jpg",
+            input_message_content=InputTextMessageContent("/stop"),
+        ),
+        InlineQueryResultArticle(
+            title="Shuffle Stream",
+            description="Shuffle the queued tracks list.",
+            thumb_url="https://telegra.ph/file/04b2f1f1c808dc49db35b.jpg",
+            input_message_content=InputTextMessageContent("/shuffle"),
+        ),
+        InlineQueryResultArticle(
+            title="Seek Stream",
+            description="Seek the ongoing stream to a specific duration.",
+            thumb_url="https://telegra.ph/file/04b2f1f1c808dc49db35b.jpg",
+            input_message_content=InputTextMessageContent("/seek 10"),
+        ),
+        InlineQueryResultArticle(
+            title="Loop Stream",
+            description="Loop the current playing music. | Usage: /loop [enable|disable]",
+            thumb_url="https://telegra.ph/file/04b2f1f1c808dc49db35b.jpg",
+            input_message_content=InputTextMessageContent("/loop 3"),
+        ),
+    ]
+)
